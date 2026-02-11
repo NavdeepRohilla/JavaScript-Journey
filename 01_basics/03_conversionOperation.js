f2b7fabf256e2efca5fd98c5d0f6daa5d1622e08
Navdeep Rohilla <navdeeprohilla03@gmail.com>
2026-02-11 14:17:02 +0530

    Conversion of Data Types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,45 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) // It is not a number . JavaScript m number m convert ho jayega but number ho ye jaruri nhi h . It wil be NaN(Not a number)
+
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+// empty => false
+// "Navdeep" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
