26fc212476561f552c36d290c22bd48eaa7aaa59
Navdeep Rohilla <navdeeprohilla03@gmail.com>
2026-02-11 12:41:07 +0530

    Learned Variables will update more later in ReadMe

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello Navdeep")   // We are using node.js , not browser
+
+console.log(3
+    +
+    3) // Code readability should be high .
+
+console.log("Navdeep")
+
+let name = "Navdeep"
+let age = 18
+let isLoggedIn = false
+
+
+
+/*
+These are Primitive DataTypes:-
+    number => 2 to power 53
+    bigInt
+    string => ""
+    boolean => True/False
+    null => stand alone value (It's a representation of empty value)
+    undefined = > Value is not assigned
+    symbol => used to find uniqueness 
+
+
+*/
+
+//Objects
+
+console.log(typeof null);
+console.log(typeof undefined)
+
+
+// Type of Function is used to check the type of the variable.
