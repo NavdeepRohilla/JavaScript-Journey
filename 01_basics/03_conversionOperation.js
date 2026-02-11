dff308b17fcc20cbdca64c2a33ceecc6c84a5b08
Navdeep Rohilla <navdeeprohilla03@gmail.com>
2026-02-11 14:36:55 +0530

    more conversions of Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,7 +4,7 @@
 // console.log(typeof(score));
 
 // let valueInNumber = Number(score)
-// console.log(typeof valueInNumber)
+// // console.log(typeof valueInNumber)
 // console.log(valueInNumber) // It is not a number . JavaScript m number m convert ho jayega but number ho ye jaruri nhi h . It wil be NaN(Not a number)
 
 // let score = null
@@ -18,12 +18,12 @@
 
 let score = true
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 
 // "33" => 33
@@ -33,7 +33,7 @@ console.log(valueInNumber)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 
 // empty => false
@@ -41,5 +41,59 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+
+
+// ********************************** OPERATIONS ***********************************
+
+let value = 3
+let negValue = -value
+//  console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-6);
+// console.log(2*6);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Navdeep"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(2+2+"2");
+
+// Yrr in industries hm aise kbhi nhi likhte sirf smjhne k liye h .. 
+// If you want ki ye kyu hota h then you can visit the documentation ToPrimitive part.
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+// console.log(typeof(""));
+// console.log(typeof(0));
+// console.log(typeof(null));
+// console.log(typeof(undefined));
+// console.log(typeof(NaN));
+// console.log(typeof(Infinity));
+// console.log(typeof(NaN));
+// console.log(typeof(NaN));
+
+
+let num1 , num2 , num3 = 2 + 2
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+++gameCounter
+console.log(gameCounter);
+
