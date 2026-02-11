72af98b8c0c7dd549d850f9fd21388ebb13eba62
Navdeep Rohilla <navdeeprohilla03@gmail.com>
2026-02-11 15:46:46 +0530

    dataTypes Summary

---
+++ datatypes-Summary.js
@@ -0,0 +1,41 @@
+// Primitive DataTyes
+
+// 7 Types => String , Number , Boolean , null , undefined , Symbol , BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+const bigNumber = 545351546513254135351n
+
+
+console.log(typeof bigNumber);
+
+
+
+const heros = ["Shaktiman" , "Naagraj" , "Doga"]
+let myObj = {
+    name: "Navdeep",
+    age: 19,
+
+}
+
+const myFunction = function () {
+    console.log("Hello World");
+    
+}
+
+
+
+// Reference Type (Non Primitive)
+
+// Arrays , Objects , Functions
