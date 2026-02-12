4bfd4617eec782782d8f5391d882c68250737ed7
Navdeep Rohilla <navdeeprohilla03@gmail.com>
2026-02-12 14:59:14 +0530

    Learned Stacks & Heaps

--- 01_basics/datatypes-Summary.js
+++ 01_basics/datatypes-Summary.js
@@ -12,15 +12,23 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 
 const bigNumber = 545351546513254135351n
 
 
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 
 
+
+
+
+
+
+// Reference Type (Non Primitive)
+
+// Arrays , Objects , Functions
 
 const heros = ["Shaktiman" , "Naagraj" , "Doga"]
 let myObj = {
@@ -34,8 +42,34 @@ const myFunction = function () {
     
 }
 
+// console.log(typeof anotherId);
 
 
-// Reference Type (Non Primitive)
 
-// Arrays , Objects , Functions
+// **************************************************************************************
+
+// Stack (Primitive) , Heap (Non-Primitive)
+
+let myYoutubename = "CeodeWithNavdeepdotcom"
+let anotherName = "Chaiaurcode"
+
+
+console.log(myYoutubename)
+console.log(anotherName);
+
+let userOne = {
+    email: "navdeep@google.com",
+    upi: "navdeep@ybl",
+}
+
+let userTwo = userOne
+userTwo.email = "ananya@google.com"
+userTwo.upi = "ananya@ybl"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+console.log(userOne.upi);
+console.log(userTwo.upi);
+
+
