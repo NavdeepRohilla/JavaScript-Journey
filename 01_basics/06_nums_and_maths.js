2150b25cd62e6fbf5af758b20a2858850ee9f7fa
Navdeep Rohilla <navdeeprohilla03@gmail.com>
2026-02-12 22:38:31 +0530

    Learned about nums and Math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,38 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2)) // Always use in e commerce website.
+
+const otherNumber = 1236.8564
+
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+
+// ++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-5))
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+// console.log(Math.min(2 , 87 , 5 , 7 , 6 , 3 , 1))
+
+console.log(Math.random());
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min )
